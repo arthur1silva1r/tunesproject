fe6dea1c835e8622db3182b2d622bf649d67ef16
Arthur da Silva Rodrigues <arthur1silva1r@gmail.com>
2021-12-21 21:58:08 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Album from './pages/Album';
+import NotFound from './pages/NotFound';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <>
+        <p>TrybeTunes</p>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" render={ () => <Login /> } exact />
+            <Route path="/search" render={ () => <Search /> } exact />
+            <Route path="/album/:id" render={ () => <Album /> } exact />
+            <Route path="/favorites" render={ () => <Favorites /> } exact />
+            <Route path="/profile" render={ () => <Profile /> } exact />
+            <Route path="/profile/edit" render={ () => <ProfileEdit /> } exact />
+            <Route component={ NotFound } />
+          </Switch>
+        </BrowserRouter>
+      </>
+    );
   }
 }
 

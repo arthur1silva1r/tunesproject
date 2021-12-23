10d681611c8b8fb5e609466d39e992a65a7b4fa3
Arthur da Silva Rodrigues <arthur1silva1r@gmail.com>
2021-12-23 17:49:17 -0300

    requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,12 +1,52 @@
 import React, { Component } from 'react';
 import Header from './Header';
 
+const minimalArtistNameLength = 2;
+
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      arthistName: '',
+      disabledBtn: true,
+    };
+  }
+
+  artistNameChange = ({ target: { value } }) => {
+    if (value.length >= minimalArtistNameLength) {
+      this.setState({
+        arthistName: value,
+        disabledBtn: false,
+      });
+    }
+
+    this.setState({
+      arthistName: value,
+    });
+  }
+
   render() {
+    const { arthistName, disabledBtn } = this.state;
     return (
       <>
         <Header />
         <div data-testid="page-search">Search</div>
+        <form>
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            placeholder="Nome do Arthista"
+            value={ arthistName }
+            onChange={ this.artistNameChange }
+          />
+          <button
+            type="submit"
+            data-testid="search-artist-button"
+            disabled={ disabledBtn }
+          >
+            Pesquisar
+          </button>
+        </form>
       </>
     );
   }

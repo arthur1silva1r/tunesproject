a7a80a602b31004cb123c8502359896b76075994
Arthur da Silva Rodrigues <arthur1silva1r@gmail.com>
2021-12-22 21:20:39 -0300

    requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,81 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router';
+import { createUser } from '../services/userAPI';
+
+const minimalLengthInput = 3;
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      btnIsDisable: true,
+      loading: false,
+      redirect: false,
+    };
+
+    this.loginAction = this.loginAction.bind(this);
+  }
+
+  handleInputChange = ({ target }) => {
+    const { value } = target;
+    if (value.length >= minimalLengthInput) {
+      this.setState(() => ({
+        name: value,
+        btnIsDisable: false,
+      }));
+    }
+
+    this.setState(() => ({
+      name: value,
+    }));
+  }
+
+  async loginAction() {
+    const { name } = this.state;
+    this.setState({
+      loading: true,
+    });
+
+    await createUser({ name });
+
+    this.setState({
+      redirect: true,
+      loading: false,
+    });
+  }
+
   render() {
+    const { name, btnIsDisable, loading, redirect } = this.state;
+    if (redirect) {
+      return <Redirect to="/search" />;
+    }
     return (
-      <div data-testid="page-login">Login</div>
+      loading ? (
+        <div data-testid="page-login">
+          <p>Carregando...</p>
+        </div>)
+        : (
+          <div data-testid="page-login">
+            <input
+              type="text"
+              id="nameInput"
+              name="userName"
+              data-testid="login-name-input"
+              placeholder="Nome"
+              value={ name }
+              onChange={ this.handleInputChange }
+            />
+            <button
+              data-testid="login-submit-button"
+              type="button"
+              onClick={ this.loginAction }
+              disabled={ btnIsDisable }
+            >
+              Entrar
+            </button>
+          </div>
+        )
     );
   }
 }

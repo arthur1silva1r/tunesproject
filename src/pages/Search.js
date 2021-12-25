ea01b3a1694ca2d7e4b2b87b98c2ef073e7d83b1
Arthur da Silva Rodrigues <arthur1silva1r@gmail.com>
2021-12-25 10:30:59 -0300

    requisito 6-corrigindo erro na pesquisa

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -10,6 +10,7 @@ class Search extends Component {
     super();
     this.state = {
       arthistName: '',
+      search: '',
       disabledBtn: true,
       loading: false,
       artistExb: [], // artista que será exibido na tela
@@ -21,11 +22,13 @@ class Search extends Component {
     if (value.length >= minimalArtistNameLength) {
       this.setState({
         arthistName: value,
+        search: value,
         disabledBtn: false,
       });
     } else {
       this.setState({
         arthistName: value,
+        search: value,
       });
     }
   }
@@ -40,60 +43,64 @@ class Search extends Component {
       loading: false,
       artistExb: dataArtist,
       req: true,
+      arthistName: '',
     });
   }
 
   render() {
-    const { arthistName, disabledBtn, loading, req, artistExb } = this.state;
-    const stringName = `Resultado de álbuns de: ${arthistName}`;
+    const { arthistName, disabledBtn, loading, req, artistExb, search } = this.state;
+    const stringName = `Resultado de álbuns de: ${search}`;
     return (
       <>
         <Header />
-        <div data-testid="page-search">Search</div>
-        {
-          loading ? (<p>Carregando...</p>) : (
-            <form>
-              <input
-                type="text"
-                data-testid="search-artist-input"
-                placeholder="Nome do Arthista"
-                value={ arthistName }
-                onChange={ this.artistNameChange }
-              />
-              <button
-                type="submit"
-                data-testid="search-artist-button"
-                disabled={ disabledBtn }
-                onClick={ this.searchBtnAction }
-              >
-                Pesquisar
-              </button>
-            </form>
-          )
-        }
-        {
-          !req ? null : <h4>{ stringName }</h4>
-        }
-        {
-          req && (
-            <div>
-              {artistExb.length === 0 ? <h1>Nenhum álbum foi encontrado</h1> : (
-                artistExb.map((album, index) => (
-                  <div key={ index }>
-                    <img src={ album.artworkUrl100 } alt="imagem do album" />
-                    <h4>{album.collectionName}</h4>
-                    <Link
-                      to={ `album/${album.collectionId}` }
-                      data-testid={ `link-to-album-${album.collectionId}` }
-                    >
-                      {album.artistName}
-                    </Link>
-                  </div>
-                ))
-              )}
-            </div>
-          )
-        }
+        <div data-testid="page-search">
+          Search
+          {
+            loading ? (<p>Carregando...</p>) : (
+              <form>
+                <input
+                  type="text"
+                  data-testid="search-artist-input"
+                  placeholder="Nome do Arthista"
+                  value={ arthistName }
+                  onChange={ this.artistNameChange }
+                />
+                <button
+                  type="submit"
+                  data-testid="search-artist-button"
+                  disabled={ disabledBtn }
+                  onClick={ this.searchBtnAction }
+                >
+                  Pesquisar
+                </button>
+              </form>
+            )
+          }
+          {
+            req && (
+              <>
+                <p>{stringName}</p>
+                <div>
+                  {artistExb.length === 0 ? <h1>Nenhum álbum foi encontrado</h1> : (
+                    artistExb.map((album, index) => (
+                      <div key={ index }>
+                        <img src={ album.artworkUrl100 } alt="imagem do album" />
+                        <h4>{album.collectionName}</h4>
+                        <Link
+                          to={ `album/${album.collectionId}` }
+                          data-testid={ `link-to-album-${album.collectionId}` }
+                        >
+                          {album.artistName}
+                        </Link>
+                      </div>
+                    ))
+                  )}
+                </div>
+
+              </>
+            )
+          }
+        </div>
       </>
     );
   }

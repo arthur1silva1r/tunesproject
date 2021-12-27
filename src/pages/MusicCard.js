f5c115d0801f8ead58595f642d24ee86af276b3b
Arthur da Silva Rodrigues <arthur1silva1r@gmail.com>
2021-12-27 01:15:00 -0300

    requisito 8

--- src/pages/MusicCard.js
+++ src/pages/MusicCard.js
@@ -1,28 +1,78 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { addSong } from '../services/favoriteSongsAPI';
 
 class MusicCard extends Component {
+  constructor() {
+    super();
+    this.state = {
+      favoritada: false,
+      loading: false,
+    };
+  }
+
+  favoritarMusica = async ({ target }) => {
+    this.setState({
+      favoritada: true,
+      loading: true,
+    });
+
+    await addSong(target.value);
+
+    this.setState({
+      loading: false,
+    });
+  }
+
   render() {
-    const { previewUrl, trackName } = this.props;
+    const { favoritada, loading } = this.state;
+    const { previewUrl, trackName, trackId } = this.props;
+    //     if (loading) {
+    //       return (
+    // <span></span>
+    //       );
+    //     }
     return (
-      <div>
-        <h5>{ trackName }</h5>
-        <audio data-testid="audio-component" src={ previewUrl } controls>
-          <track kind="captions" />
-          O seu navegador não suporta o elemento
-          <code>
-            audio
-          </code>
-          .
-        </audio>
-      </div>
+      loading ? (<h1>Carregando...</h1>)
+        : (
+          <div>
+            <h3>{ trackName }</h3>
+            <audio data-testid="audio-component" src={ previewUrl } controls>
+              <track kind="captions" />
+              O seu navegador não suporta o elemento
+              <code>
+                audio
+              </code>
+              .
+            </audio>
+            <label htmlFor="favorite">
+              Favorita
+              <input
+                type="checkbox"
+                name="favorite"
+                id="favorite"
+                data-testid={ `checkbox-music-${trackId}` }
+                onClick={ this.favoritarMusica }
+                value={ trackId }
+                defaultChecked={ favoritada }
+              />
+            </label>
+          </div>
+        )
     );
   }
 }
 
 MusicCard.propTypes = {
-  trackName: PropTypes.string.isRequired,
-  previewUrl: PropTypes.string.isRequired,
+  trackName: PropTypes.string,
+  previewUrl: PropTypes.string,
+  trackId: PropTypes.number,
+};
+
+MusicCard.defaultProps = {
+  trackName: '',
+  previewUrl: '',
+  trackId: 0,
 };
 
 export default MusicCard;
